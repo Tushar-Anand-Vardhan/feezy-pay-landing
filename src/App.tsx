bc976378d22b9874a0c5ce8c24b4e88219b337b3
Tushar Vardhan <tushar220.tv@gmail.com>
2025-11-20 14:59:37 +0530

    adding pages - 1

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import Privacy from "./pages/Privacy"
 import NotFound from "./pages/NotFound";
-import Refund from "./pages/Refund";
-import About from "./pages/About";
 import Compliance from "./pages/Compliance";
 import Security from "./pages/Security";
 import Terms from "./pages/Terms";
 import Contact from "./pages/Contact";
+import Privacy from "./pages/privacy";
+import Refund from "./pages/refund";
+import About from "./pages/about";
 
 const queryClient = new QueryClient();
 
